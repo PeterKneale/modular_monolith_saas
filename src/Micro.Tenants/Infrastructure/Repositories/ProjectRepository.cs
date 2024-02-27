1edca826ad9e71d567e24ed6ae06c3e522a07315
Peter Kneale <peterkneale@gmail.com>
2024-02-27 21:44:10 +1100

    add proper exceptions and better use of dapper

--- src/Micro.Tenants/Infrastructure/Repositories/ProjectRepository.cs
+++ src/Micro.Tenants/Infrastructure/Repositories/ProjectRepository.cs
@@ -11,56 +11,57 @@ internal class ProjectRepository(ConnectionFactory connections) : IProjectReposi
     public async Task CreateAsync(Project project, CancellationToken token)
     {
         const string sql = $"INSERT INTO {ProjectsTable} ({IdColumn}, {OrganisationIdColumn}, {NameColumn}) VALUES (@Id, @OrganisationId, @Name)";
-        var row = new Row
-        {
-            Id = project.Id.Value,
-            OrganisationId = project.OrganisationId.Value,
-            Name = project.Name.Value
-        };
         using var con = connections.CreateConnection();
-        await con.ExecuteAsync(new CommandDefinition(sql, row, cancellationToken: token));
+        await con.ExecuteAsync(new CommandDefinition(sql, new Row
+        {
+            Id = project.Id,
+            OrganisationId = project.OrganisationId,
+            Name = project.Name
+        }, cancellationToken: token));
     }
 
     public async Task UpdateAsync(Project project, CancellationToken token)
     {
         const string sql = $"UPDATE {ProjectsTable} SET {NameColumn} = @Name WHERE {IdColumn} = @Id";
-        var row = new Row
-        {
-            Id = project.Id.Value,
-            Name = project.Name.Value
-        };
         using var con = connections.CreateConnection();
-        await con.ExecuteAsync(new CommandDefinition(sql, row, cancellationToken: token));
+        await con.ExecuteAsync(new CommandDefinition(sql, new Row
+        {
+            Id = project.Id,
+            Name = project.Name
+        }, cancellationToken: token));
     }
 
     public async Task<Project?> GetAsync(ProjectId id, CancellationToken token)
     {
         const string sql = $"SELECT * FROM {ProjectsTable} WHERE {IdColumn} = @Id";
         using var con = connections.CreateConnection();
-        var row = await con.QuerySingleOrDefaultAsync<Row>(new CommandDefinition(sql, new { Id = id.Value }, cancellationToken: token));
-        return row == null ? null : Map(row);
+        var row = await con.QuerySingleOrDefaultAsync<Row>(new CommandDefinition(sql, new Row
+        {
+            Id = id
+        }, cancellationToken: token));
+        return Map(row);
     }
 
     public async Task<Project?> GetAsync(ProjectName name, CancellationToken token)
     {
         const string sql = $"SELECT * FROM {ProjectsTable} WHERE {NameColumn} = @Name";
         using var con = connections.CreateConnection();
-        var row = await con.QuerySingleOrDefaultAsync<Row>(new CommandDefinition(sql, new { Name = name.Value }, cancellationToken: token));
-        return row == null ? null : Map(row);
+        var row = await con.QuerySingleOrDefaultAsync<Row>(new CommandDefinition(sql, new Row
+        {
+            Name = name
+        }, cancellationToken: token));
+        return Map(row);
     }
 
-    private static Project Map(Row row)
-    {
-        var id = new ProjectId(row.Id);
-        var name = new ProjectName(row.Name);
-        var organisationId = new OrganisationId(row.OrganisationId);
-        return new Project(id, organisationId, name);
-    }
+    private static Project? Map(Row? row) =>
+        row != null
+            ? new Project(row.Id, row.OrganisationId, row.Name)
+            : null;
 
     private class Row
     {
-        public Guid Id { get; init; }
-        public Guid OrganisationId { get; init; }
-        public string Name { get; init; } = null!;
+        public ProjectId Id { get; init; } = null!;
+        public OrganisationId OrganisationId { get; init; } = null!;
+        public ProjectName Name { get; init; } = null!;
     }
 }
