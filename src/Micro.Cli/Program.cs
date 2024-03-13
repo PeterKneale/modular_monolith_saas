967cf80f4617ac03743da3b41bee69d7d7f39dc8
Peter Kneale <peterkneale@gmail.com>
2024-03-13 14:51:04 +1100

    tidy

--- src/Micro.Cli/Program.cs
+++ src/Micro.Cli/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Micro.Common.Application;
 using Micro.Common.Infrastructure.Integration.Bus;
+using Micro.Tenants.Application.Organisations.Commands;
 using Micro.Tenants.Application.Users.Commands;
 using Micro.Translations.Application.Commands;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,11 +8,12 @@ using Microsoft.Extensions.Logging;
 
 var organisationId = Guid.NewGuid();
 var userId = Guid.NewGuid();
+var projectId = Guid.NewGuid();
 var accessor = new Accessor
 {
     User = new UserExecutionContext(new UserId(userId)),
     Organisation = new OrganisationExecutionContext(new OrganisationId(organisationId)),
-    Project = new ProjectExecutionContext(new ProjectId(Guid.NewGuid()))
+    Project = new ProjectExecutionContext(new ProjectId(projectId))
 };
 
 var configuration = new ConfigurationBuilder()
@@ -34,6 +36,7 @@ TranslationModuleStartup.Start(accessor, configuration, bus, logs, true);
 
 var tenantsModule = services.GetRequiredService<ITenantsModule>();
 var translationModule = services.GetRequiredService<ITranslationModule>();
-await tenantsModule.SendCommand(new RegisterUser.Command(Guid.NewGuid(), "x", "x", $"x{Guid.NewGuid().ToString()}@example.com", "x"));
+await tenantsModule.SendCommand(new RegisterUser.Command(userId, "x", "x", $"x{Guid.NewGuid().ToString()}@example.com", "x"));
+await tenantsModule.SendCommand(new CreateOrganisation.Command(organisationId, "x"));
 await tenantsModule.SendCommand(new ProcessOutboxCommand());
 await translationModule.SendCommand(new ProcessInboxCommand());
---
+++ src/Micro.Translations/Infrastructure/Integration/Handlers/OrganisationCreatedHandler.cs
@@ -0,0 +1,12 @@
+using Micro.Tenants.IntegrationEvents;
+
+namespace Micro.Translations.Infrastructure.Integration.Handlers;
+
+public class OrganisationCreatedHandler(ILogger<OrganisationCreatedHandler> logs) : INotificationHandler<OrganisationCreated>
+{
+    public Task Handle(OrganisationCreated notification, CancellationToken cancellationToken)
+    {
+        logs.LogInformation($"Organisation created: {notification.OrganisationId}");
+        return Task.CompletedTask;
+    }
+}
