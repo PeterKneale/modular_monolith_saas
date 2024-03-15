ea43f441a52603e65ba54a697b234bac800c8e10
Peter Kneale <peterkneale@gmail.com>
2024-03-15 23:13:54 +1100

    x

--- run-docker-compose-test.sh
+++ run-docker-compose-test.sh
@@ -4,23 +4,32 @@ set -e
 echo "##########################################"
 echo "Running unit tests"
 echo "##########################################"
-docker compose -f docker-compose-unit.yml build
-docker compose -f docker-compose-unit.yml up \
+docker compose --progress quiet -f docker-compose-unit.yml build
+docker compose --progress quiet -f docker-compose-unit.yml up \
+  --force-recreate \
+  --remove-orphans \
+  --no-log-prefix \
   --abort-on-container-exit \
   --exit-code-from unit-tests
 
 echo "##########################################"
 echo "Running integration tests"
 echo "##########################################"
-docker compose -f docker-compose-integration.yml build
-docker compose -f docker-compose-integration.yml up \
+docker compose --progress quiet -f docker-compose-integration.yml build
+docker compose --progress quiet -f docker-compose-integration.yml up \
+  --force-recreate \
+  --remove-orphans \
+  --no-log-prefix \
   --abort-on-container-exit \
   --exit-code-from integration-tests
   
 echo "##########################################"
 echo "Running acceptance tests"
 echo "##########################################"
-docker compose -f docker-compose-acceptance.yml build
-docker compose -f docker-compose-acceptance.yml up \
+docker compose --progress quiet -f docker-compose-acceptance.yml build
+docker compose --progress quiet -f docker-compose-acceptance.yml up \
+  --force-recreate \
+  --remove-orphans \
+  --no-log-prefix \
   --abort-on-container-exit \
   --exit-code-from acceptance-tests
