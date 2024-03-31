d7619fc13fa308e49a01a52b29ccd3b88f06d2c7
Peter Kneale <peterkneale@gmail.com>
2024-03-31 21:21:49 +1100

    add log

--- run-docker-compose-test.sh
+++ run-docker-compose-test.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "##########################################"
 echo "Running unit tests"
-echo "{Running unit tests}" >> $GITHUB_STEP_SUMMARY
+echo "Running unit tests" >> $GITHUB_STEP_SUMMARY
 echo "##########################################"
 docker compose --progress quiet -f docker-compose-unit.yml build
 docker compose --progress quiet -f docker-compose-unit.yml up \
@@ -15,7 +15,7 @@ docker compose --progress quiet -f docker-compose-unit.yml up \
 
 echo "##########################################"
 echo "Running integration tests"
-echo "{Running integration Tests}" >> $GITHUB_STEP_SUMMARY
+echo "Running integration Tests" >> $GITHUB_STEP_SUMMARY
 echo "##########################################"
 docker compose --progress quiet -f docker-compose-integration.yml build
 docker compose --progress quiet -f docker-compose-integration.yml up \
@@ -27,7 +27,7 @@ docker compose --progress quiet -f docker-compose-integration.yml up \
   
 echo "##########################################"
 echo "Running acceptance tests"
-echo "{Running acceptance Tests}" >> $GITHUB_STEP_SUMMARY
+echo "Running acceptance Tests" >> $GITHUB_STEP_SUMMARY
 echo "##########################################"
 docker compose --progress quiet -f docker-compose-acceptance.yml build
 docker compose --progress quiet -f docker-compose-acceptance.yml up \
