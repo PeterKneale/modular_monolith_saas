fbcd7140cbe2401675d6e68aa59398299b2a298c
Peter Kneale <peterkneale@gmail.com>
2024-05-01 23:20:02 +1000

    fix logging

--- src/Micro.Proxy/Program.cs
+++ src/Micro.Proxy/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddLogging(c => { c.AddSimpleConsole(x => { x.SingleLine = true; }); });
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
